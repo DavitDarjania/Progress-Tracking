7f8a28992724d2baffe7d4ff8362a847ed668b01
DavitDarjania <151033720+DavitDarjania@users.noreply.github.com>
2025-03-21 03:07:39 +0400

    added api fetching for tasks with adding in html, and some functionalities of filtering cards.

--- scripts/api.js
+++ scripts/api.js
@@ -28,18 +28,46 @@ export default {
     })
   },
 
-    async getEmployees(){
-      return await fetch("https://momentum.redberryinternship.ge/api/employees", {
-        method: "GET",
-        headers: {
-          Accept: 'application/json',
-          Authorization: 'Bearer 9e7883a9-1113-45e6-8c94-d46503c125e3',
-        }
-      }).then((response) => {
-        return response.json();
-      }).then((data) => {
-        console.log(data);
-        return data;
-      })
-}
+  async getEmployees(){
+    return await fetch("https://momentum.redberryinternship.ge/api/employees", {
+      method: "GET",
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer 9e7883a9-1113-45e6-8c94-d46503c125e3',
+      }
+    }).then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      return data;
+    })
+  },
+  async getAllStatuses(){
+    return await fetch("https://momentum.redberryinternship.ge/api/statuses", {
+      method: "GET",
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer 9e7883a9-1113-45e6-8c94-d46503c125e3',
+      }
+    }).then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      return data;
+    })
+  },
+  async getAllTasks(){
+    return await fetch("https://momentum.redberryinternship.ge/api/tasks", {
+      method: "GET",
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer 9e7883a9-1113-45e6-8c94-d46503c125e3',
+      }
+    }).then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      return data;
+    })
+  },
 }
